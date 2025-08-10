ae7731f99eb0ac4594385b67e59392f8b7efe8bc
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-08-11 00:07:48 +0530

    api update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://interproject-9rsw.onrender.com/', 
+  baseURL: 'https://interproject-9rsw.onrender.com/api', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
