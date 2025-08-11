602e65e8c2c1637c1e99b12fc5f1fd5a8048b099
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-08-11 08:19:15 +0530

    new api point

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://interproject-9rsw.onrender.com/api', 
+  baseURL: 'https://interproject-nn9i.onrender.com/api', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
